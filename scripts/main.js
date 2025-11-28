f11e6bde26e603735018013f7bf989c28d29a550
Pavel Zabelin <126048380+trier111@users.noreply.github.com>
2025-11-28 22:30:29 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+const myButton = document.querySelector("button");
+const myImage = document.querySelector("img");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Go to hell, ${myName}`;
+}
+
+myButton.addEventListener("click", () => {
+  setUserName();
+  changeImage();
+});
+
+function changeImage() {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/skeleton.jpg") {
+    myImage.setAttribute("src", "images/go-to-hell.jpg");
+  }
+}
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
